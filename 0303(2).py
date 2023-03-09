1859b3b77860149f56ea05725f2a99fd8d943e7c
melfaberlioz <sofi.horizon@gmail.com>
2023-03-09 12:04:00 +0200

    Initial Commit

--- 0303(2).py
+++ 0303(2).py
@@ -2,9 +2,6 @@
 # for / while
 
 for i in range(5):
-    print('Hi', end=' ') # 0 1 2 3 4 Hi
-
-# Перевірити, яке число просте (використовуючи цикл for).
+    print('Hi', end=' ')
 
 # print(1, 'Hi') # 1 Hi\n
-# print('\t-----------------')
